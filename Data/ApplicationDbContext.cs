957da1e480bf508b14669a6ee29f099ddc3ae5b3
DhruvShukla1120 <141140954+DhruvShukla1120@users.noreply.github.com>
2023-08-17 14:24:14 +0530

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DevOpsGroupProjects.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevOpsGroupProjects.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+
+        public DbSet<Product> products { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace DevOpsGroupProjects.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string ProductDescription { get; set; }
+
+        public int Price { get; set; }
+    }
+}
+
