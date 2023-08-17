92934c41cfbe386181425e79b1e7dcfba932395a
KhansahabMonis <141143087+KhansahabMonis@users.noreply.github.com>
2023-08-17 15:22:07 +0530

    Add files via upload

---
+++ ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace returnorder.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ OrderController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace returnorder.Controllers
+{
+    
+    
+        public class OrderController : Controller
+        {
+            // GET: Order/ReturnAndRefund
+            public ActionResult ReturnAndRefund(int orderId)
+            {
+                // Here you could retrieve the order details from your data source using the orderId
+                // For this example, let's assume you have an Order model with appropriate properties.
+
+                // Replace this with actual code to retrieve order details
+                var order = new Order
+                {
+                    OrderId = orderId,
+                    OrderDate = DateTime.Now,
+                    // Other order properties...
+                };
+
+                return View(order);
+            }
+
+            // POST: Order/SubmitReturn
+            [HttpPost]
+            public ActionResult SubmitReturn(int orderId, string reason)
+            {
+                // Here you could process the return request and update the order status accordingly
+                // You could also log the return reason and any other relevant information.
+
+                // Replace this with actual code to process the return
+                // For this example, let's assume you update the order status to "Return Requested"
+                // and log the return reason.
+
+                // Update order status
+                // UpdateReturnStatus(orderId, "Return Requested");
+
+                // Log the return reason
+                // LogReturnReason(orderId, reason);
+
+                ViewBag.Message = "Return request submitted successfully.";
+                return View();
+            }
+        }
+
+    internal class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
